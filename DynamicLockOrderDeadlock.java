fa48dabfceade786d4c9afac021c45d1cca8b50a
Adedapo Adeyemi <adedapo025@gmail.com>
2025-01-30 18:31:02 +0100

    added example of dynamic lock order deadlock with notes

---
+++ DynamicLockOrderDeadlock.java
@@ -0,0 +1,30 @@
+public class DynamicLockOrderDeadlock {
+    public void transferMoney(Account fromAccount, Account toAccount, double amount){
+        /*
+         * This is also deadlock prone though it might initially appear as if all the threads acquire their lock in the same order
+         * but in fact, the lock order here depends on the order of the arguments passed to transferMoney.
+         * Deadlock can actually occur if two threads call transferMoney at the same time, one transferring from X to Y, the other transferring
+         * from Y to X.
+         * 
+         * So the first thread acquires a lock on X, the second thread acquires lock on Y first, but the first thread needs Y which is already held by the second thread
+         * and the second thread now needs X which is already held by the first thread, so hence a deadlock occurs.
+         */
+        
+        synchronized(fromAccount){
+            synchronized(toAccount){
+                fromAccount.debit(amount);
+                toAccount.credit(amount);
+            }
+        }
+    }
+
+    // class definition
+    public static class Account {
+        private int id;
+        public int getID(){
+            return id;
+        }
+        public void debit(double amount){}
+        public void credit(double amount){}
+    }
+}
